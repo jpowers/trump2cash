15db7c7a02873b77a99e27266d10964465bf07f1
Jay Powers <jay@vermonster.com>
2017-02-16 13:42:31 -0500

    Load ENV from dotenv

--- trading.py
+++ trading.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import settings
 from datetime import datetime
 from datetime import timedelta
 from lxml.etree import Element
--- twitter.py
+++ twitter.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import settings
 from os import getenv
 from simplejson import loads
 from Queue import Queue
